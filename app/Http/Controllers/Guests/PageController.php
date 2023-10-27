6d4171a762c47f0a4783521fd2499afbe0bf2c6e
FabioVasi <vasifabio96@gmail.com>
2023-10-27 17:49:38 +0200

    add

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
 use App\Models\Movie;
-use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        dd(Movie::all());
-        return view('welcome');
+        //dd(Movie::all());
+
+        return view('welcome', ['movies' => Movie::all()]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,24 @@
 
 @section('content')
 
-    <h1>Welcome</h1>
+    <section>
+
+        <div class="container">
+            <h1>Welcome</h1>
+            <div class="row row-cols-5">
+                @forelse ($movies as $movie)
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h3>{{$movie->title}}</h3>
+                            </div>
+                        </div>
+                    </div>
+                @endforealse
+            </div>
+        </div>
+        
+    </section>
+    
 
 @endsection
