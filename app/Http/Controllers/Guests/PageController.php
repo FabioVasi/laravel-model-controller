2aac86b717ffdf3ba7eacb13b084b667e0bfa495
FabioVasi <vasifabio96@gmail.com>
2023-10-27 17:39:56 +0200

    add layouts

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
+        dd(Movie::all());
         return view('welcome');
     }
 }
